f2d7e536a801a03f4fa46fb977eabefc2f398289
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-02-22 13:39:26 +0000

    remove examples Box components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import './App.css';
-import Box from './components/Box';
 import { Canvas } from '@react-three/fiber';
 import GuardianText from './components/GuardianText';
 
@@ -9,9 +7,7 @@ function App() {
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <GuardianText position={[2, -2, 0]} />
+      <GuardianText position={[-2, -2, 0]} />
     </Canvas>
   );
 }
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -6,16 +6,16 @@ function Box(props: ThreeElements['mesh']) {
   const mesh = useRef<THREE.Mesh>(null!)
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
-  useFrame((state, delta) => (mesh.current.rotation.y += delta))
+  useFrame((_state, delta) => (mesh.current.rotation.y += delta))
   
   return (
     <mesh
       {...props}
       ref={mesh}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
