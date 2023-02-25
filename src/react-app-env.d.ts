f41fb1c565bad1532bcceb5283dc97c1b7c4bd16
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-02-25 15:48:58 +0000

    declare textGeometry three type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+
+
 /// <reference types="react-scripts" />
+
+// Add types to ThreeElements elements so primitives pick up on it
+declare module '@react-three/fiber' {
+    interface ThreeElements {
+      textGeometry: Object3DNode<TextGeometry, typeof TextGeometry>
+    }
+  }
